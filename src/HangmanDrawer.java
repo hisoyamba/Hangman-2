bc68266353f08d95186f95a915f2893293699522
hisoyamba <167796407+hisoyamba@users.noreply.github.com>
2025-09-28 19:24:09 +0700

    Update HangmanDrawer.java

--- src/HangmanDrawer.java
+++ src/HangmanDrawer.java
@@ -2,7 +2,7 @@ public class HangmanDrawer {
     private static HangmanStage[] hangman = HangmanStage.values();
 
     public static void draw(int attempt) {
-        if (attempt > 0 && attempt < hangman.length) {
+        if (attempt > 0 && attempt <= hangman.length) {
             System.out.println(hangman[attempt - 1].getHangmanStage());
         }
 
